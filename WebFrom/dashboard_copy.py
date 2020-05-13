a75c9fab28521c6649a62931ba905bf0451cc2fa
Rahul Dhakecha <rahuldhakecha@Rahuls-MacBook-Pro.local>
2020-05-13 13:54:58 +0530

    Orders updated for DN- trying to rectify dashboard for DN trial 12

--- WebFrom/dashboard_copy.py
+++ WebFrom/dashboard_copy.py
@@ -139,12 +139,13 @@ dash_app3.layout = dn_order_layout
                      State('order_project_value', 'value'),
                      State('order_remarks', 'value'),
                      State('order_comp_location', 'value'),
-                     State('order_add_contact_div', 'children')]
+                     State('order_add_contact_div', 'children'),
+                     State('orders_table', 'data')]
                   )
 def update_order_values(submit_clicks, close_clicks, order_enquiry_key, client_dropdown, row_id, clickData_scope, clickData_status, order_key, order_date, order_po_no,
                         order_project_description, order_scope_of_work, order_client_name, order_client_location, order_existing_client,
                         order_order_no, order_file_no, order_status, order_project_incharge, order_raj_group_office,
-                        order_project_value, order_remarks, order_comp_location, add_contact_div_value):
+                        order_project_value, order_remarks, order_comp_location, add_contact_div_value, rows):
     print("Coming Here for trigger1")
     ctx = dash.callback_context
     ctx_msg = json.dumps({
